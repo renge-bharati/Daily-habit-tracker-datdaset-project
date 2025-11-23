0db8139df244cdd5b0ef74e0250cb33e33454b76
renge-bharati <rengebharati@gmail.com>
2025-11-23 14:12:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pickle
 
 st.title("Daily Habit Completion Prediction")
 
-model = pickle.load(open("model.pkl", "rb"))
+model = pickle.load(open("best_model.joblib", "rb"))
 
 st.write("Enter Habit Details to Predict if it will be Completed")
 
